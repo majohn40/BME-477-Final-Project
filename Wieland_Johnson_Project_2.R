55f5f22fe66745056e04f96cc27880b63d97c135
Megan Johnson <megan.ann.johnson7@gmail.com>
2020-10-26 18:02:00 -0700

    Add file upload interface

--- Wieland_Johnson_Project_2.R
+++ Wieland_Johnson_Project_2.R
@@ -26,54 +26,49 @@ labs <- read_tsv("LabsCorePopulatedTable.txt", col_names = TRUE)
 corePopulated <- read_tsv("PatientCorePopulatedTable.txt", col_names = TRUE)
 patientData = list(corePopulated, admissions, diagnoses, labs)
 
-######################################################################################################
-############################# Statistical Analysis ###################################################
-######################################################################################################
-
-#T Test
-analyses_t_test <- function(compareVariable, group1, group2) {
-  group1PatientID <- as.data.frame(subset(patientData[[4]], patientData[[4]]$PatientGender == group1))
-  group1PatientIDtest <- unique(group1PatientID$PatientID)
-  variableCompare1 <- subset(patientData[[3]], patientData[[3]]$PatientID %in% group1PatientIDtest == TRUE
-                             & patientData[[3]]$LabName == variableOfInterest)
-  
-  group2PatientID <- as.data.frame(subset(patientData[[4]], patientData[[4]]$PatientGender == group2))
-  group2PatientIDtest <- unique(group2PatientID$PatientID)
-  variableCompare2 <- subset(patientData[[3]], patientData[[3]]$PatientID %in% group2PatientIDtest == TRUE
-                             & patientData[[3]]$LabName == compareVariable)
-  
-  return(t.test(variableCompare1$LabValue,variableCompare2$LabValue ))
-}
-
-possible_options <- function(patientData) {
-  emptyList <- vector(mode = "list", length = length(patientData))
-  emptyDataFrame <- data.frame(matrix(nrow = 0, ncol = 0))
-  for (i in 1:length(patientData)) {
-    for (j in 2:dim(patientData[[i]])[2]) {
-      print(c(i,j))
-      emptyDataFrame <- as.data.frame(t(rbind.fill(as.data.frame(emptyDataFrame), as.data.frame(t((unique(patientData[[i]])[j]))))))
-    }
-    emptyList[i] <- emptyDataFrame
-  }
-  
-  return(emptyList)
-}
-
-test_function <- function(patientData) {
-  
-  print("test")
-}
-
-variableOfInterest <- readline(prompt = "Enter variable to analyze:")
-analyses_t_test(variableOfInterest, "Male", "Female")
-test <- possible_options(patientData)
+# ######################################################################################################
+# ############################# Statistical Analysis ###################################################
+# ######################################################################################################
+# 
+# #T Test
+# analyses_t_test <- function(compareVariable, group1, group2) {
+#   group1PatientID <- as.data.frame(subset(patientData[[4]], patientData[[4]]$PatientGender == group1))
+#   group1PatientIDtest <- unique(group1PatientID$PatientID)
+#   variableCompare1 <- subset(patientData[[3]], patientData[[3]]$PatientID %in% group1PatientIDtest == TRUE
+#                              & patientData[[3]]$LabName == variableOfInterest)
+#   
+#   group2PatientID <- as.data.frame(subset(patientData[[4]], patientData[[4]]$PatientGender == group2))
+#   group2PatientIDtest <- unique(group2PatientID$PatientID)
+#   variableCompare2 <- subset(patientData[[3]], patientData[[3]]$PatientID %in% group2PatientIDtest == TRUE
+#                              & patientData[[3]]$LabName == compareVariable)
+#   
+#   return(t.test(variableCompare1$LabValue,variableCompare2$LabValue ))
+# }
+# 
+# possible_options <- function(patientData) {
+#   emptyList <- vector(mode = "list", length = length(patientData))
+#   emptyDataFrame <- data.frame(matrix(nrow = 0, ncol = 0))
+#   for (i in 1:length(patientData)) {
+#     for (j in 2:dim(patientData[[i]])[2]) {
+#       print(c(i,j))
+#       emptyDataFrame <- as.data.frame(t(rbind.fill(as.data.frame(emptyDataFrame), as.data.frame(t((unique(patientData[[i]])[j]))))))
+#     }
+#     emptyList[i] <- emptyDataFrame
+#   }
+#   
+#   return(emptyList)
+# }
+# 
+# test_function <- function(patientData) {
+#   
+#   print("test")
+# }
+# 
+# #variableOfInterest <- readline(prompt = "Enter variable to analyze:")
+# #analyses_t_test(variableOfInterest, "Male", "Female")
+# test <- possible_options(patientData)
 
 #----------------------------------------------------------------------------------------------------
-
-
-
-
-
 ui <- fluidPage(navbarPage("VERITAS",
                  tabPanel("Home",
                           h3("Welcome to VERITAS"),
@@ -90,13 +85,20 @@ ui <- fluidPage(navbarPage("VERITAS",
                                          options = list(maxItems = 1)),#caps to only 1 input, but does not autofill the box
                           ),
                  
-                 tabPanel("Input Data"),
+                 tabPanel("Input Data",
+                          h3("Upload Patient Data by Filetype"),
+                          fileInput("admissions", "Admission Data", accept = ".txt"),
+                          fileInput("diagnoses", "Diagnostic Data", accept = ".txt"),
+                          fileInput("labs", "Lab Data", accept = ".txt"),
+                          fileInput("core_populated", "Core Populated Data", accept = ".txt"),
+                          
+                 ),
                  
                  tabPanel("Analysis Tool")
                  )
 )
 
-server <- function(input, output) {}
+server <- function(input, output) { }
 
 shinyApp(ui = ui, server = server)
 
